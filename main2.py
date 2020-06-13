41825e0d0f0abcab5eb4e514d4f69cb58144f395
GZM <gimzimean@gmail.com>
2020-06-13 12:11:06 +0900

    text, pic 완성:

--- main2.py
+++ main2.py
@@ -1,7 +1,7 @@
 from telegram.ext import Updater, MessageHandler, Filters
 
 updater = Updater(
-    token='1249064364:AAFr2nlFtCd0CAQoifExahswHSQ_4GDBfeU', use_context=True)
+    token='1249064364:AAFr2nlFtCd0CAQoifExahswHSQ_4GDBfeU')
 
 dispatcher = updater.dispatcher
 updater.start_polling()  # 주기적으로 텔레그램 서버에 접속해서 종현이로부터 새로운 메새지가 ㅣㅆ으면 받아오라
@@ -15,7 +15,7 @@ def handler(bot, update):
         bot.send_message(chat_id=chat_id, text="지미니 생각ㅎㅎ")
     elif '사진' in text:
         bot.send_photo(chat_id=chat_id, photo=open(
-            '/img/Screenshot_108.png', 'rb'))  # rb : 바이너리 형식
+            'img/Screenshot_8.png', 'rb'))  # rb : 바이너리 형식
     else:
         bot.send_message(chat_id=chat_id, text="몰롸")
 
